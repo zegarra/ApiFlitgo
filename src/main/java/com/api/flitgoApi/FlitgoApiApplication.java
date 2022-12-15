594c7e37d7d11c4191f97336aa356d5a2df1a0c1
FEZE1660ti <alonsozegarra2690@gmail.com>
2022-12-15 10:47:17 -0500

    commit test

---
+++ src/main/java/com/api/flitgoApi/FlitgoApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.flitgoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlitgoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlitgoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/flitgoApi/FlitgoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.flitgoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlitgoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
